50433c86a4f22d919c25d7c2347a4b527363ac10
Daniel Rovira Martínez <theDeskIT@gmail.com>
2022-06-03 10:34:36 +0200

    Metadatos

---
+++ metadatos.py
@@ -0,0 +1,34 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+
+def metadatos(ruta):
+        
+
+    #Leemos la imagen
+
+    mi_imagen = Image.open(ruta)
+
+    #Extraemos los metadatos
+
+    exif_data = mi_imagen.getexif()
+
+    #Iteramos para sacar todos los metadatos
+
+    dataEncontrada=False
+
+    i=0
+
+    for tagId in exif_data:
+        #Obtenemos la etiqueta nombre
+        tag = TAGS.get(tagId, tagId)
+        data = exif_data.get(tagId)
+
+        
+        print(f"{tag:16} {data}")
+
+    
+
+
+
+metadatos("/Users/daniel/Documents/02_programas/python/tmp/input/IMG_0277.jpeg")
